8d31f0f3516f91a62b758b1a4964ca3863560fdc
tinamariathomas <codertina@gmail.com>
2016-02-16 18:14:38 +0530

    Initial Commit

---
+++ server.go
@@ -0,0 +1,19 @@
+package main
+
+
+import(
+	"net/http"
+	"log"
+	"os"
+)
+
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+	log.Println("Server started: http://localhost:" + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
