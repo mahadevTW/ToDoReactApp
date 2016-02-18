5c38216c4a2c8f8bc88e52a17bd7ad9c9600771b
tinamariathomas <codertina@gmail.com>
2016-02-18 16:09:06 +0530

    Dummy App

---
+++ public/scripts/todoapp.js
@@ -0,0 +1,12 @@
+var ToDoApp = React.createClass({
+    render: function () {
+        return (
+            <div>
+                Hi
+            </div>
+        )
+    }
+});
+
+
+ReactDOM.render(<ToDoApp />, document.getElementById('content'));
