c70293db369b26d45540e4bb412b700c7dcc2c91
Tina-Maria <codertina@gmail.com>
2016-02-24 13:59:28 +0530

    Added ToDoInput Component

--- public/scripts/todoapp.js
+++ public/scripts/todoapp.js
@@ -2,11 +2,22 @@ var ToDoApp = React.createClass({
     render: function () {
         return (
             <div>
-                Hi
+                <ToDoInput></ToDoInput>
             </div>
         )
     }
 });
+
+var ToDoInput = React.createClass({
+    render: function () {
+        <form onsubmit={this.handleSubmit}>
+            <input type="text" placeholder="ToDo" onChange={this.handleChange}/>
+            <input type="submit" value="Post"/>
+        </form>
+    }
+});
+
+
 
 
 ReactDOM.render(<ToDoApp />, document.getElementById('content'));
