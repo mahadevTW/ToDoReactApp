b950e690aedf216b2b8fd59a0b62cdf9ad046dc7
tinamariathomas <codertina@gmail.com>
2016-04-01 10:13:17 +0530

    Making Text Display a Dumb Component

--- app/js/src/components/TextDisplay.js
+++ app/js/src/components/TextDisplay.js
@@ -1,24 +1,7 @@
-var Reflux = require('reflux');
-var ToDoStore= require('./../stores/todostore');
-
-const listener = Reflux.ListenerMixin;
-
 var TextDisplay = React.createClass({
-    getInitialState: function () {
-        return ({text: "empty"});
-    },
-    componentDidMount: function(){
-        listener.listenTo(ToDoStore, this.onUpdateList);
-    },
-    onUpdateList: function (result) {
-        this.setState({
-            text: result.text
-        });
-        console.log(result.action);
-    },
     render: function () {
         return (
-            <div>{this.state.text}</div>
+            <div>{this.props.text}</div>
         )
     }
 });
--- app/js/src/components/ToDoAppComp.js
+++ app/js/src/components/ToDoAppComp.js
@@ -1,11 +1,27 @@
+var Reflux = require('reflux');
 var TextDisplay= require('./TextDisplay');
 var ToDoInput= require('./ToDoInput');
+var ToDoStore= require('./../stores/todostore');
+
+const listener = Reflux.ListenerMixin;
 
 var ToDoApp = React.createClass({
+    getInitialState: function () {
+        return ({text: "empty"});
+    },
+    componentDidMount: function(){
+        listener.listenTo(ToDoStore, this.onUpdateList);
+    },
+    onUpdateList: function (result) {
+        this.setState({
+            text: result.text
+        });
+        console.log(result.action);
+    },
     render: function () {
         return (
             <div>
-                <TextDisplay/>
+                <TextDisplay text={this.state.text}/>
                 <ToDoInput/>
             </div>
         )
