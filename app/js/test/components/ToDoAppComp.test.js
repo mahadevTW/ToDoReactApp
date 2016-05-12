1a2ea91d6914b6c1044998f5ad63218c35e67608
tinamariathomas <codertina@gmail.com>
2016-05-12 23:31:24 +0530

    Test - ToDoApp listens to Store and triggers state change

--- app/js/test/components/ToDoAppComp.test.js
+++ app/js/test/components/ToDoAppComp.test.js
@@ -3,6 +3,9 @@ var JsDom = require("../utils/jsdom");
 var expect = require('chai').expect;
 var ReactTestUtils = require("react-addons-test-utils");
 var ToDoComponent = require("../../src/components/ToDoAppComp");
+var ToDoStore = require("../../src/stores/todostore");
+var sinon = require("sinon");
+
 describe('ToDoComp', function() {
         it('should render TextDisplay and ToDoInput', function () {
             let renderer = ReactTestUtils.createRenderer();
@@ -10,5 +13,14 @@ describe('ToDoComp', function() {
             let component = renderer.getRenderOutput();
             expect(component.props.children[0].type.displayName).to.equal('TextDisplay');
             expect(component.props.children[1].type.displayName).to.equal('ToDoInput');
-        });      
+        });   
+        it.only('should update List when notified', function(){
+            var component = ReactTestUtils.renderIntoDocument(<ToDoComponent/>);
+            let data={
+                text:"Hello"
+            }
+            ToDoStore.onUpdateList("Hello");
+            expect(component.state.text).to.be.equal("Hello");
+                                                             
+        })   
 });
